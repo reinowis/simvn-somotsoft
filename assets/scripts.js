f0e61606a9f9689b91935cef37e97950fa22a147
reinomai <mousewis@gmail.com>
2020-12-13 00:44:40 +0700

    edit: homepage

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+$(document).ready(function(){
+  $(".owl-carousel").each(function (index) {
+    var items_no = $(this).data('slides') || 3;
+    var autoplay = $(this).data('autoplay') || false;
+      $(this).owlCarousel({ // use $(this)
+          items: items_no,
+          autoplay: autoplay
+      });
+  });
+});
