403ad4ecfc01a746682aa71f364afe5f42364918
reinomai <mousewis@gmail.com>
2020-12-13 23:40:20 +0700

    edit: homepage/4

--- assets/scripts.js
+++ assets/scripts.js
@@ -32,12 +32,20 @@ $(document).ready(function(){
   /** End of Carousel */
   /** Sticky the header */
   // get the sticky element
-});
-const stickyElm = document.querySelector('header');
-
-  const observer = new IntersectionObserver(function(e){
-    for (let i = 0; i < e.length; i++){
-      $(e[i].target).toggleClass('is-sticky', e[0].intersectionRatio < 1);
-    }
-  }, {threshold: [0]});
-  observer.observe(stickyElm);
+  var stickyElem = $(".header");
+//   var observer = new IntersectionObserver(function(e){
+//       for (let i = 0; i < e.length; i++){
+//         if (!e[i].isIntersecting){
+//           $(e[i].target).parents('.header').addClass('is-sticky');
+//         }
+//         else{
+//           if (e[i].target.getBoundingClientRect().top < 0 ) {
+//             $(e[i].target).parents('.header').removeClass('is-sticky');
+//           }
+//         }
+//       }
+//     }, {threshold: [0], rootMargin: '-50px 0px -55%'});
+//   for (let i = 0; i < stickyElem.length; i++){
+//     observer.observe(stickyElem[i]);
+//   }
+// });
