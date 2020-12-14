57ef7f66eaa573d0260bb8a99a57e53ac6010840
reinomai <60995396+reinomai@users.noreply.github.com>
2020-12-14 15:53:10 +0700

    edit: responsive on homepage

--- assets/scripts.js
+++ assets/scripts.js
@@ -32,20 +32,20 @@ $(document).ready(function(){
   /** End of Carousel */
   /** Sticky the header */
   // get the sticky element
-  var stickyElem = $(".header");
-//   var observer = new IntersectionObserver(function(e){
-//       for (let i = 0; i < e.length; i++){
-//         if (!e[i].isIntersecting){
-//           $(e[i].target).parents('.header').addClass('is-sticky');
-//         }
-//         else{
-//           if (e[i].target.getBoundingClientRect().top < 0 ) {
-//             $(e[i].target).parents('.header').removeClass('is-sticky');
-//           }
-//         }
-//       }
-//     }, {threshold: [0], rootMargin: '-50px 0px -55%'});
-//   for (let i = 0; i < stickyElem.length; i++){
-//     observer.observe(stickyElem[i]);
-//   }
-// });
+  var stickyElem = $(".header__mobile__collapse");
+  var observer = new IntersectionObserver(function(e){
+      for (let i = 0; i < e.length; i++){
+        if (e[i].isIntersecting){
+          $("body").append("<div class='header__mobile__backdrop'></div>");
+          $("body").toggleClass('is-backdrop', true);
+        }
+        else{
+          $(".header__mobile__backdrop").remove();
+          $("body").toggleClass('is-backdrop', false);
+        }
+      }
+    }, {threshold: [0], rootMargin: '-50px 0px -55%'});
+  for (let i = 0; i < stickyElem.length; i++){
+    observer.observe(stickyElem[i]);
+  }
+});
