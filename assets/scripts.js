78611aff860b64ee160b5f7352f3cd4f79022192
reinomai <60995396+reinomai@users.noreply.github.com>
2020-12-17 15:34:36 +0700

    add: popup, edit: animations

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,24 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
-$(document).ready(function(){
-  /** Set up Carousel */
-  $(".owl-carousel").each(function (index) {
-    var items_no = $(this).data('slides') || 3;
-    var autoplay = $(this).data('autoplay') || false;
-    var nav = $(this).data('nav') || false;
-    var dots = $(this).data('dots') || false;
-    var slidesMobile = $(this).data('slides-mobile') || items_no;
-    var slidesTablet = $(this).data('slides-tablet') || items_no;
-    var loop = $(this).data('loop') || false;
-      $(this).owlCarousel({ // use $(this)
+  $('[data-toggle="tooltip"]').tooltip();
+  $(document).ready(function(){
+    /** Set up Carousel */
+    $(".owl-carousel").each(function (index) {
+      var items_no = $(this).data('slides') || 3;
+      var autoplay = $(this).data('autoplay') || false;
+      var nav = $(this).data('nav') || false;
+      var dots = $(this).data('dots') || false;
+      var slidesMobile = $(this).data('slides-mobile') || items_no;
+      var slidesTablet = $(this).data('slides-tablet') || items_no;
+      var loop = $(this).data('loop') || false;
+      var autoplaySpeed = false;
+      var smartSpeed = $(this).data('smart-speed') || 1000;
+      $(this).owlCarousel({
           autoplay: autoplay,
           nav: nav,
           dots: dots,
           loop: loop,
+          autoplaySpeed: autoplaySpeed,
+          smartSpeed: smartSpeed,
           responsive: {
             0:{
               items: slidesMobile
@@ -28,45 +31,61 @@ $(document).ready(function(){
             }
           }
       });
-  });
-  /** End of Carousel */
-  /** Sticky the header */
-  // get the sticky element
-  var stickyElem = $(".header__mobile__collapse");
-  var observer = new IntersectionObserver(function(e){
-      for (let i = 0; i < e.length; i++){
-        if (e[i].isIntersecting){
-          $("body").toggleClass('is-backdrop', true);
-          $("body").append("<div class='header__mobile__backdrop'></div>");
+    });
+    /** End of Carousel */
+    /** Sticky the header */
+    // get the sticky element
+    var stickyElem = $(".header__mobile__collapse");
+    var observer = new IntersectionObserver(function(e){
+        for (let i = 0; i < e.length; i++){
+          if (e[i].isIntersecting){
+            $("body").toggleClass('is-backdrop', true);
+            $("body").append("<div class='header__mobile__backdrop'></div>");
+          }
+          else{
+            $("body").toggleClass('is-backdrop', false);
+            $(".header__mobile__backdrop").remove();
+          }
         }
-        else{
-          $("body").toggleClass('is-backdrop', false);
-          $(".header__mobile__backdrop").remove();
-        }
-      }
-    }, {threshold: [0], rootMargin: '-50px 0px -55%'});
-  for (let i = 0; i < stickyElem.length; i++){
-    observer.observe(stickyElem[i]);
-  }
-  /** Set up the custom select */
-  $(".select-customized").selectmenu({
-    classes: {
-      "ui-selectmenu-button" : "select-customized__button",
-      "ui-selectmenu-menu": "select-customized__dropdown"
+      }, {threshold: [0], rootMargin: '-50px 0px -55%'});
+    for (let i = 0; i < stickyElem.length; i++){
+      observer.observe(stickyElem[i]);
     }
-  });
-  /** Set up the zoom out */
-  $("a.img-zoom").zoom();
-  /** Set up smooth scroll */
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } // End if
+    /** Set up the custom select */
+    $(".select-customized").selectmenu({
+      classes: {
+        "ui-selectmenu-button" : "select-customized__button",
+        "ui-selectmenu-menu": "select-customized__dropdown"
+      }
+    });
+    /** Set up the zoom out */
+    $("a.img-zoom").zoom();
+    /** Set up smooth scroll */
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } // End if
+    });
   });
 });
+
+/** Set up category popup */
+function showCategoryPopup(){
+  $("#category-popup").modal({
+    backdrop: 'static',
+    // Prevent click outside to close modal
+    keyboard: false
+    // Prevnt escape key to close modal
+  });
+}
+/** Handling data for popup goes here*/
+$('#category-popup').on('shown.bs.modal	', function (e) {
+  // do something...
+})
+/** Edn of Set up category popup */
